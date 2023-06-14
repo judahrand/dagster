ca046153cd1e33e71aa3079b4fa0fecf35961c71
Judah Rand <17158624+judahrand@users.noreply.github.com>
2023-06-14 01:05:20 +0100

    Force libraries to be parsed

--- python_modules/libraries/dagster-databricks/dagster_databricks/databricks.py
+++ python_modules/libraries/dagster-databricks/dagster_databricks/databricks.py
@@ -373,7 +373,7 @@ class DatabricksJobRunner:
                     {
                         "new_cluster": new_cluster,
                         "existing_cluster_id": existing_cluster_id,
-                        "libraries": libraries,
+                        "libraries": [jobs.Library.from_dict(lib) for lib in libraries],
                         **task,
                     },
                 )
